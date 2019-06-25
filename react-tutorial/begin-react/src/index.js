2899492b5b0dfb0d9583d762e73e4ccdf2783bdc
yykdev <assavictory@gmail.com>
2019-06-26 00:06:50 +0900

    3. 나의 첫번째 리액트 컴포넌트

--- react-tutorial/begin-react/src/index.js
+++ react-tutorial/begin-react/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// 페이지에서 ID가 root인 엘리먼트를 찾아서 리액트 App을 넣겠다. => index.html : <div id="root"></div>
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
