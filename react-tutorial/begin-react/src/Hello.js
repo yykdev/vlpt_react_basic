8083a421c3b6abb555ddfd629cb8db0c77f5f0db
yykdev <assavictory@gmail.com>
2019-07-01 00:01:13 +0900

    props로 css 사용법 - 구조분해 할당 방식

--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -1,10 +1,10 @@
 import React from 'react'; // react를 사용 하겠다!!
 
 // 컴포넌트 명은 카멜 케이스로 
-function Hello(props) {
+function Hello({name, color}) {
     return <div style={{
-        color: props.color
-    }}>안녕하세요. {props.name}</div>
+        color: color
+    }}>안녕하세요. {name}</div>
 }
 
 // Hello 컴포넌트를 사용 할 수 있도록 내보내 주겠다.
