bbe84bcdc59d7a5647af92b335030c5040521226
yykdev <assavictory@gmail.com>
2019-06-30 23:59:57 +0900

    props로 css 사용법 - 기본

--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -2,7 +2,9 @@ import React from 'react'; // react를 사용 하겠다!!
 
 // 컴포넌트 명은 카멜 케이스로 
 function Hello(props) {
-    return <div>안녕하세요. {props.name}</div>
+    return <div style={{
+        color: props.color
+    }}>안녕하세요. {props.name}</div>
 }
 
 // Hello 컴포넌트를 사용 할 수 있도록 내보내 주겠다.
