1fde1de72be753320c1865cec22b80035214415c
yykdev <assavictory@gmail.com>
2019-07-01 00:21:21 +0900

    조건부 렌더링

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -5,10 +5,14 @@ import Wrapper from './Wrapper';
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial={true}/>
       <Hello color="pink" />
     </Wrapper>
   );
 }
+
+// boolean 의 경우 값은 넣지 않고 이름만 셋팅 할 경우 true 로 인정 된다
+// isSpecial={true} => true
+// isSpecial => true
 
 export default App;
--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -1,11 +1,23 @@
 import React from 'react'; // react를 사용 하겠다!!
 
 // 컴포넌트 명은 카멜 케이스로 
-function Hello({name, color}) {
-    return <div style={{
-        color
-    }}>안녕하세요. {name}</div>
+function Hello({name, color, isSpecial}) {
+    return (
+        <div style={{
+            color
+        }}>
+            {isSpecial && <b>*</b>}
+            {isSpecial? '스페셜!!! ':'낫스페셜... '}
+            안녕하세요. {name}
+        </div>
+    )
 }
+
+// 특정 조건에서만 출력하고자 하는 경우 and 연산자 ( && ) 사용하는 것이 좋다.
+// {isSpecial && <b>*</b>}
+
+// 조건의 결과 값에 따라 출력 결과가 다를 경우 삼항연산자를 사용하는게 낫다.
+// {isSpecial? '스페셜!!! ':'낫스페셜... '}
 
 // 컴포넌트명.defaultProps : 특정 키의 값이 없을 경우 사용할 기본 값 설정
 Hello.defaultProps = {
