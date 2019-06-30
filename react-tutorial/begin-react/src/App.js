59bf378f30535feca58cd24547ccbf9094a0c73a
yykdev <assavictory@gmail.com>
2019-06-30 23:58:10 +0900

    props 사용법 - 기본

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import Hello from './Hello';
 
-import './App.css';
-
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1ren',
-  };
-
   return (
-    <>
-      <Hello/>
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+      <Hello name="react" />
   );
 }
 
--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -1,8 +1,8 @@
 import React from 'react'; // react를 사용 하겠다!!
 
 // 컴포넌트 명은 카멜 케이스로 
-function Hello() {
-    return <div>안녕하세요</div>
+function Hello(props) {
+    return <div>안녕하세요. {props.name}</div>
 }
 
 // Hello 컴포넌트를 사용 할 수 있도록 내보내 주겠다.
