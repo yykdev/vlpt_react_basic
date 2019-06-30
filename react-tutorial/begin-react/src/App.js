06042b6a6c32b75b6d5ac935f29fd4afa4bd7804
yykdev <assavictory@gmail.com>
2019-07-01 00:07:43 +0900

    Wrapper 컴포넌트 생성 및 자동완성 불러오기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Hello name="react" color="red" />
       <Hello color="pink" />
-    </>
+    </Wrapper>
   );
 }
 
---
+++ react-tutorial/begin-react/src/Wrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Wrapper() {
+    const style = {
+        border: '2px solid black',
+        padding: 16
+    }
+
+    return <div style={style}></div>
+}
+
+export default Wrapper;
