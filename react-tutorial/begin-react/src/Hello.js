28c7f3114785defbf8667a85deba5b500bcb368b
yykdev <assavictory@gmail.com>
2019-07-01 00:03:59 +0900

    컴포넌트명.defaultProps : 특정 키의 값이 없을 경우 사용할 기본 값 설정

--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -3,9 +3,13 @@ import React from 'react'; // react를 사용 하겠다!!
 // 컴포넌트 명은 카멜 케이스로 
 function Hello({name, color}) {
     return <div style={{
-        color: color
+        color
     }}>안녕하세요. {name}</div>
 }
 
+// 컴포넌트명.defaultProps : 특정 키의 값이 없을 경우 사용할 기본 값 설정
+Hello.defaultProps = {
+    name: '이름없음'
+}
 // Hello 컴포넌트를 사용 할 수 있도록 내보내 주겠다.
 export default Hello;
