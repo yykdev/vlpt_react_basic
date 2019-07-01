530ec232597c80cfc1398602765d105b66e3d32b
yykdev <assavictory@gmail.com>
2019-07-01 10:52:41 +0900

    07. useState 를 통해 컴포넌트에서 바뀌는 값 관리하기

---
+++ react-tutorial/begin-react/src/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Counter() {
+    return (
+        <div>
+            <h1>0</h1>
+            <button>+</button>
+            <button>-</button>
+        </div>
+    )
+}
+
+export default Counter;
