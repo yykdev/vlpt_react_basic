511fbc716f2b3456c0aaaf8add98061166426b52
yykdev <assavictory@gmail.com>
2019-07-01 11:00:13 +0900

    07. useState 를 통해 컴포넌트에서 바뀌는 값 관리하기

--- react-tutorial/begin-react/src/Counter.js
+++ react-tutorial/begin-react/src/Counter.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Counter() {
+
+    const [number, setNumber] = useState(0);
+
+    const onIncrease = () => {
+        setNumber(number + 1);
+    }
+
+    const onDecrease = () => {
+        setNumber(number - 1);
+    }
+
     return (
         <div>
-            <h1>0</h1>
-            <button>+</button>
-            <button>-</button>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
         </div>
     )
 }
+
+// useState(기본값) 를 사용하여 바뀌는 값을 관리 할 수 있다.
+// useState 에 전달 되는 인자 값은 기본 값으로 설정 된다.
 
 export default Counter;
